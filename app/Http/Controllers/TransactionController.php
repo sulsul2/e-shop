59de282f88565220c4ce42dc6c1bb97f96d11e93
sulsul2 <110590472+sulsul2@users.noreply.github.com>
2023-07-16 12:49:25 +0700

    finished contoller

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -13,8 +13,25 @@ class TransactionController extends Controller
         
         $transactions = Transaction::where('user_id',$id)->get();
 
-        dd($transactions);
-
         return view('history',[$transactions]);
+    }
+
+    function addTransaction(Request $request){
+        $request->validate([
+            'nama_barang' => 'required',
+            'harga_barang' => 'required',
+            'jumlah_barang' => 'required',
+            'nama_penerima'=>'required',
+            'alamat_pengiriman'=>'required',
+        ]);
+        $transactions = Transaction::create([
+            'user_id' => Auth::id(),
+            'nama_barang' => $request->nama_barang,
+            'harga_barang' => $request->harga_barang,
+            'jumlah_barang' => $request->jumlah_barang,
+            'total_harga' => $request->jumlah_barang * $request->harga_barang,
+            'nama_penerima'=>$request->nama_penerima,
+            'alamat_pengiriman'=>$request->alamat_pengiriman,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
